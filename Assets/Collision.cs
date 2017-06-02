65c7cffdd897aa0408ec87a9081222fafe68c864
Srivatsan <srivatsan@skytousolution.com>
2017-06-02 12:21:42 +0530

    Made Some Changes

---
+++ Assets/Collision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject alien;
+    Rigidbody rb;
+	void Start () {
+        rb = alien.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if(collision.gameObject.tag=="Cow")
+        {
+            rb.AddForce(50f, 10f, 0, ForceMode.Impulse);
+            Debug.Log("Cow Fucked");
+        }
+    }
+}
---
+++ Assets/CowMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowMovement : MonoBehaviour {
+    public float speed;
+    public Animator cowAnim;
+	// Use this for initialization
+	void Start () {
+        cowAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            cowAnim.SetFloat("Speed",1f);
+        }
+        else
+        {
+            cowAnim.SetFloat("Speed", 0f);
+        }
+
+
+        if (Input.GetKey(KeyCode.Space))
+            cowAnim.SetTrigger("Attack");
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(0, -1, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(0, 1, 0);
+
+    }
+}
